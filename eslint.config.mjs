0dfa7b6bda929db50a309cd2ca077a2e2d1cf60b
auxilia <auxilia606@gmail.com>
2025-09-13 10:09:35 +0900

    Feat: 프로젝트 초기 구성

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -19,6 +20,53 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+  },
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            // Packages `react` related packages come first.
+            [
+              "^next",
+              "^react",
+              "^@mui",
+              "recharts",
+              "^@tanstack",
+              "^@dnd-kit",
+              "quill",
+              "^prettier",
+              "dayjs",
+            ],
+            ["eslint", "path", "url"],
+            // Internal packages.
+            [
+              "^@/app",
+              "^@/pages",
+              "^@/widgets",
+              "^@/features",
+              "^@/entities",
+              "^@/shared",
+              // Side effect imports.
+              "^\\u0000",
+              // Parent imports. Put `..` last.
+              "^\\.\\.(?!/?$)",
+              "^\\.\\./?$",
+              // Other relative imports. Put same-folder imports and `.` last.
+              "^\\./(?=.*/)(?!/?$)",
+              "^\\.(?!/?$)",
+              "^\\./?$",
+              // Style imports.
+              "^.+\\.?(css)$",
+            ],
+          ],
+        },
+      ],
+    },
   },
 ];
 
